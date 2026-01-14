3324aa4045cc9f03968eecfa6d0b32d24b2a4ba3
FRANKF5 <fabian.frank@roche.com>
2026-01-14 13:57:40 +0100

    reset backend

--- Backend-Repo/Backend-Code-backend/Backend-Code-backend/uniprojekt/src/main/java/com/urlaubsplaner/uniprojekt/bl/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.urlaubsplaner.uniprojekt.bl;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
